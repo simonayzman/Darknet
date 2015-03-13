152087c46169988673f5c6a062646b3eda4a3b05
Raymond Liang <Raymond@Liang.local>
2015-03-13 15:03:56 -0400

    Basic network template scripts. Require game logic to be added. Will work on creating more templates.

---
+++ Lobby.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lobby : MonoBehaviour {
+	private const string typeName = "DarkNet";
+	private const string gameName = "RoomName";
+	
+	private bool isRefreshingHostList = false;
+	private HostData[] hostList;
+	
+	public GameObject playerPrefab;
+	
+	void OnGUI() {
+		if (!Network.isClient && !Network.isServer) {
+			// Start server button.
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server")) {
+				StartServer();
+			}
+			// Refresh host list button.
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts")) {
+				RefreshHostList();
+			}
+			// Display rooms on server if any.
+			if (hostList != null) {
+				for (int i = 0; i < hostList.Length; i++) {
+					// Room button to join.
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName)) {
+						JoinServer(hostList[i]);
+					}
+				}
+			}
+		}
+	}
+	
+	private void StartServer() {
+		//Network.InitializeServer(int MaxNumIncomingConnections, int ListenPort, bool useNAT)
+		Network.InitializeServer(20, 25000, !Network.HavePublicAddress());
+		//MasterServer.RegisterHost(string gameTypeName, string gameName, string comment)
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+	
+	// Called whenever Network.InitializeServer() is invoked and completed.
+	void OnServerInitialized() {
+		SpawnPlayer();
+	}
+	
+	// Called upon every frame if MonoBehaviour is enabled.
+	void Update() {
+		if (isRefreshingHostList && MasterServer.PollHostList().Length > 0) {
+			// Set false so that it does not continuous fetch hostList.
+			isRefreshingHostList = false;
+			// Checked for latest host list rceived by RequestHostList() and store locally.
+			hostList = MasterServer.PollHostList();
+		}
+	}
+	
+	// Refresh host list.
+	private void RefreshHostList() {
+		if (!isRefreshingHostList) {
+			// Set true for update frame.
+			isRefreshingHostList = true;
+			// Obtain host list from master server.
+			MasterServer.RequestHostList(typeName);
+		}
+	}
+	
+	// Join server
+	private void JoinServer(HostData hostData) {
+		// Network.Connect(string IP, int remotePort, string password)
+		// Note HostData class can hold this information.
+		Network.Connect(hostData);
+	}
+	
+	// Executes when connected to server. Called on client.
+	void OnConnectedToServer() {
+		SpawnPlayer();
+	}
+	
+	// Script to spawn player.
+	private void SpawnPlayer() {
+		// Network.Instantiate(Object prefab, Vector3 position, Quaternion rotation, int group)
+		// Instantiate handles synchronization between clients over the network.
+		Network.Instantiate(playerPrefab, Vector3.up * 5, Quaternion.identity, 0);
+	}
+}
---
+++ LoginView.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+public class LoginView : MonoBehaviour {
+	public bool RegistrationGUI = false;
+	private string username = "";
+	private string password = "";
+	private string confirm = "";
+	private string status ="";
+
+	// Called once per frame.
+	void Update() {
+		OnGUI();
+	}
+
+	// Login Form
+	void LoginAuthentication(string USERNAME, string PASSWORD) {
+
+	}
+
+	// RegisterUser
+	void RegisterUser(string USERNAME, string PASSWORD) {
+
+	}
+
+	void OnGUI() {
+		GUI.Window(0, new Rect(Screen.width/4, Screen.height/4, Screen.width/2, Screen.height/2), LoginWindow, "Log In");
+	}
+
+	void LoginWindow(int windowId) {
+		// User + Password UI labels.
+//		GUI.Label(new Rect());
+		GUI.Label(new Rect(10, 50, 100, 30), "Username:");
+		GUI.Label(new Rect(10, 90, 100, 30), "Password:");
+		if (GUI.Button(new Rect(310, 50, 100, 30), "Log In")) {
+			LoginAuthentication(username, password);
+		}
+		username = GUI.TextField (new Rect(100, 50, 200, 30), username, 25);
+		password = GUI.PasswordField(new Rect(100, 90, 200, 30), password, "*"[0], 25);
+		if (GUI.Button(new Rect(310, 90, 100, 30), "New Account")) {
+			RegistrationGUI = true;
+		}
+		if (RegistrationGUI) {
+			GUI.Label(new Rect(10, 130, 100, 30), "Confirm password:");
+			confirm = GUI.PasswordField(new Rect(100, 130, 200, 30), confirm, "*"[0], 25);
+			if (confirm == password && GUI.Button (new Rect(310, 130, 100, 30), "Register")) {
+				RegisterUser(username, password);
+			}
+		}
+	}
+
+	void OnPlayerConnected(NetworkPlayer player) {
+		Debug.Log(username + " connected from " + player.ipAddress + ":" + player.port);
+	}
+}
