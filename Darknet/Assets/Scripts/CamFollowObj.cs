a1a704532609a38b746785c8bf3e11a4054a6705
Christian Diaz <cdiaz8369@hotmail.com>
2015-03-14 15:26:34 -0400

    Added
    -Added Test-kun sprites
    -Added CamFollow Script
    -Added Movement script
    -Updated Town-Map

---
+++ Darknet/Assets/Scripts/CamFollowObj.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+//I grabbed this script online, it works perfectly but I dont remember where I got it from.
+//Its a basic camera script. Camera follows an object, using dampTime to cause a smooth transition. 
+
+
+public class CamFollowObj : MonoBehaviour {
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	//Creates a public variable of type transform. This will be what the camera will follow.
+	//You can easily drag the object that is intended to be the target into the "target" section 
+	//in the inspection section of this component. 
+	public Transform target;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (target)
+		{   //Create a vector of the camera's point of view
+			Vector3 point = camera.WorldToViewportPoint(target.position);
+			//Position the camera away from the target along the z axis 
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			//Im not really sure
+			Vector3 destination = transform.position + delta;
+			//Transform this objects new position to the targets movements with the variables set above.  
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+	}
+}
---
+++ Darknet/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.D))
+			transform.position += new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+		if (Input.GetKey (KeyCode.A))
+			transform.position -= new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+		if (Input.GetKey (KeyCode.W))
+			transform.position += new Vector3 (0.0f, speed * Time.deltaTime, 0.0f);
+		if (Input.GetKey (KeyCode.S))
+			transform.position -= new Vector3 (0.0f, speed * Time.deltaTime, 0.0f);
+	
+
+	}
+}
