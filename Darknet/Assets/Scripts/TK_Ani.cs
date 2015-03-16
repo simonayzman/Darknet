1fa46007d90ede30c9eec11b1711b66ee5421454
Christian Diaz <cdiaz8369@hotmail.com>
2015-03-16 08:45:41 -0400

    Animation
    -Made idle animation work
    -Added lines to TK_ani.cs
    However they didnt work so it is commented out. I will work learning
    animation at a later date.

--- Darknet/Assets/Scripts/TK_Ani.cs
+++ Darknet/Assets/Scripts/TK_Ani.cs
@@ -2,14 +2,13 @@
 using System.Collections;
 
 public class TK_Ani : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+//	   if (Input.GetKey (KeyCode.D))
+//			animation.CrossFade ("Test-kun_Walk6");
+//	   else
+//			animation.CrossFade ("Test-kun_Idle5");
+		}	
 	}
-}
+
