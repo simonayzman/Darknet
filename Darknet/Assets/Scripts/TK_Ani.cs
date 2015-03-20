be805215b01a0e05e16d78be08752db7769c5625
Christian Diaz <cdiaz8369@hotmail.com>
2015-03-19 23:56:56 -0400

    Changed else if
    -changed all "else if" in TK_Ani.cs to "if"

--- Darknet/Assets/Scripts/TK_Ani.cs
+++ Darknet/Assets/Scripts/TK_Ani.cs
@@ -18,25 +18,25 @@ void Update () {
 /* 6 */	 if (Input.GetKeyDown (KeyCode.D))
 			animator.SetInteger("Tidle", 6);
 			
-/* 4 */	 else if (Input.GetKeyDown (KeyCode.A))
+/* 4 */	 if (Input.GetKeyDown (KeyCode.A))
 			animator.SetInteger("Tidle", 4);
 			
-/* 8 */	 else if (Input.GetKeyDown (KeyCode.W))
+/* 8 */	 if (Input.GetKeyDown (KeyCode.W))
 			animator.SetInteger("Tidle", 8);
 			
-/* 5 */	 else if (Input.GetKeyDown (KeyCode.S))			
+/* 5 */	 if (Input.GetKeyDown (KeyCode.S))			
 			animator.SetInteger("Tidle", 5);
 			
-/*7*/	 else if (Input.GetKeyDown (KeyCode.A) && Input.GetKey(KeyCode.W))			
+/*7*/	 if (Input.GetKeyDown (KeyCode.A) && Input.GetKey(KeyCode.W))			
 			animator.SetInteger("Tidle", 7);
 			
-/*9*/    else if (Input.GetKeyDown (KeyCode.D) && Input.GetKey(KeyCode.W))			
+/*9*/    if (Input.GetKeyDown (KeyCode.D) && Input.GetKey(KeyCode.W))			
 			animator.SetInteger("Tidle", 9);
 			
-/*1*/ 	 else if (Input.GetKeyDown (KeyCode.A) && Input.GetKey(KeyCode.S))			
+/*1*/ 	 if (Input.GetKeyDown (KeyCode.A) && Input.GetKey(KeyCode.S))			
 			animator.SetInteger("Tidle", 1);
 			
-/*3*/ 	 else if (Input.GetKeyDown (KeyCode.D) && Input.GetKey(KeyCode.S))			
+/*3*/ 	 if (Input.GetKeyDown (KeyCode.D) && Input.GetKey(KeyCode.S))			
 			animator.SetInteger("Tidle", 3);
 		
 
@@ -46,25 +46,25 @@ void Update () {
 /* 6 */	 if (Input.GetKey (KeyCode.D))
 			animator.SetInteger("Twalk", 6);
 			
-/* 4 */	 else if (Input.GetKey (KeyCode.A))
+/* 4 */	 if (Input.GetKey (KeyCode.A))
 			animator.SetInteger("Twalk", 4);
 			
-/* 8 */	 else if (Input.GetKey (KeyCode.W))
+/* 8 */	 if (Input.GetKey (KeyCode.W))
 			animator.SetInteger("Twalk", 8);
 			
-/* 5 */	 else if (Input.GetKey (KeyCode.S))			
+/* 5 */	 if (Input.GetKey (KeyCode.S))			
 			animator.SetInteger("Twalk", 5);
 			
-/*7*/	 else if (Input.GetKey (KeyCode.A) && Input.GetKey(KeyCode.W))			
+/*7*/	 if (Input.GetKey (KeyCode.A) && Input.GetKey(KeyCode.W))			
 			animator.SetInteger("Twalk", 7);
 			
-/*9*/    else if (Input.GetKey (KeyCode.D) && Input.GetKey(KeyCode.W))			
+/*9*/    if (Input.GetKey (KeyCode.D) && Input.GetKey(KeyCode.W))			
 			animator.SetInteger("Twalk", 9);
 			
-/*1*/ 	 else if (Input.GetKey (KeyCode.A) && Input.GetKey(KeyCode.S))			
+/*1*/ 	 if (Input.GetKey (KeyCode.A) && Input.GetKey(KeyCode.S))			
 			animator.SetInteger("Twalk", 1);
 			
-/*3*/ 	 else if (Input.GetKey (KeyCode.D) && Input.GetKey(KeyCode.S))			
+/*3*/ 	 if (Input.GetKey (KeyCode.D) && Input.GetKey(KeyCode.S))			
 			animator.SetInteger("Twalk", 3);		
 		
 
