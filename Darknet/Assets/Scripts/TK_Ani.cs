9aa249f01516774dcb189b97663b79c3aa546513
Christian Diaz <cdiaz8369@hotmail.com>
2015-03-23 09:14:37 -0400

    Messed up states

--- Darknet/Assets/Scripts/TK_Ani.cs
+++ Darknet/Assets/Scripts/TK_Ani.cs
@@ -4,68 +4,71 @@ using System.Collections;
 public class TK_Ani : MonoBehaviour {
 
 private Animator animator;	
+
 // Use this for initialization
 void Start(){
  animator = this.GetComponent<Animator>();
 }
-
-
+		
 
 // Update is called once per frame
 void Update () {
 
+var vertical = Input.GetAxis("Vertical");
+var horizontal = Input.GetAxis("Horizontal");
+
 //Idle Animation
-/* 6 */	 if (Input.GetKeyDown (KeyCode.D))
-			animator.SetInteger("Tidle", 6);
+/* 6 */	// if (Input.GetKeyDown (KeyCode.D))
+		//	animator.SetInteger("Tidle", 6);
 			
-/* 4 */	 if (Input.GetKeyDown (KeyCode.A))
-			animator.SetInteger("Tidle", 4);
+/* 4 */	// if (Input.GetKeyDown (KeyCode.A))
+		//	animator.SetInteger("Tidle", 4);
 			
-/* 8 */	 if (Input.GetKeyDown (KeyCode.W))
-			animator.SetInteger("Tidle", 8);
+/* 8 */	// if (Input.GetKeyDown (KeyCode.W))
+		//	animator.SetInteger("Tidle", 8);
 			
-/* 5 */	 if (Input.GetKeyDown (KeyCode.S))			
-			animator.SetInteger("Tidle", 5);
+/* 5 */	// if (Input.GetKeyDown (KeyCode.S))			
+		//	animator.SetInteger("Tidle", 5);
 			
-/*7*/	 if (Input.GetKeyDown (KeyCode.A) && Input.GetKey(KeyCode.W))			
-			animator.SetInteger("Tidle", 7);
+/*7*/	// if (Input.GetKeyDown (KeyCode.A) && Input.GetKey(KeyCode.W))			
+		//	animator.SetInteger("Tidle", 7);
 			
-/*9*/    if (Input.GetKeyDown (KeyCode.D) && Input.GetKey(KeyCode.W))			
-			animator.SetInteger("Tidle", 9);
+/*9*/   // if (Input.GetKeyDown (KeyCode.D) && Input.GetKey(KeyCode.W))			
+		//	animator.SetInteger("Tidle", 9);
 			
-/*1*/ 	 if (Input.GetKeyDown (KeyCode.A) && Input.GetKey(KeyCode.S))			
-			animator.SetInteger("Tidle", 1);
+/*1*/ 	// if (Input.GetKeyDown (KeyCode.A) && Input.GetKey(KeyCode.S))			
+		//	animator.SetInteger("Tidle", 1);
 			
-/*3*/ 	 if (Input.GetKeyDown (KeyCode.D) && Input.GetKey(KeyCode.S))			
-			animator.SetInteger("Tidle", 3);
+/*3*/ //  if (Input.GetKeyDown (KeyCode.D) && Input.GetKey(KeyCode.S))			
+		//	animator.SetInteger("Tidle", 3);
 		
 
 		
 		
 //Walking Animation
-/* 6 */	 if (Input.GetKey (KeyCode.D))
+/* 6 */	 if (horizontal < 0){
 			animator.SetInteger("Twalk", 6);
-			
-/* 4 */	 if (Input.GetKey (KeyCode.A))
+			}
+/* 4 */	 else if (horizontal > 0){
 			animator.SetInteger("Twalk", 4);
-			
-/* 8 */	 if (Input.GetKey (KeyCode.W))
+			}
+/* 8 */	 else if (vertical > 0){
 			animator.SetInteger("Twalk", 8);
-			
-/* 5 */	 if (Input.GetKey (KeyCode.S))			
+			}
+/* 5 */	 else if (vertical < 0){			
 			animator.SetInteger("Twalk", 5);
+		}
+/*7*/	 //if (Input.GetKey (KeyCode.A) && Input.GetKey(KeyCode.W))			
+		//	animator.SetInteger("Twalk", 7);
 			
-/*7*/	 if (Input.GetKey (KeyCode.A) && Input.GetKey(KeyCode.W))			
-			animator.SetInteger("Twalk", 7);
+/*9*/   // if (Input.GetKey (KeyCode.D) && Input.GetKey(KeyCode.W))			
+		//	animator.SetInteger("Twalk", 9);
 			
-/*9*/    if (Input.GetKey (KeyCode.D) && Input.GetKey(KeyCode.W))			
-			animator.SetInteger("Twalk", 9);
+/*1*/ 	// if (Input.GetKey (KeyCode.A) && Input.GetKey(KeyCode.S))			
+		//	animator.SetInteger("Twalk", 1);
 			
-/*1*/ 	 if (Input.GetKey (KeyCode.A) && Input.GetKey(KeyCode.S))			
-			animator.SetInteger("Twalk", 1);
-			
-/*3*/ 	 if (Input.GetKey (KeyCode.D) && Input.GetKey(KeyCode.S))			
-			animator.SetInteger("Twalk", 3);		
+/*3*/ 	// if (Input.GetKey (KeyCode.D) && Input.GetKey(KeyCode.S))			
+		//	animator.SetInteger("Twalk", 3);		
 		
 
 
