8774393cc10bda15a092a5859f3c2496c3092e98
Christian Diaz <cdiaz8369@hotmail.com>
2015-04-01 00:29:11 -0400

    Added FollowCube
    -Added FollowCube.cs
    Test-kun now follows the cube in the 3d world!

---
+++ Darknet/Assets/FollowCube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCube : MonoBehaviour {
+
+	public Transform target;
+	public float zOffset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate(){
+		transform.localPosition = new Vector3 (target.localPosition.x, transform.localPosition.y, target.localPosition.z+ zOffset);
+	}
+}
