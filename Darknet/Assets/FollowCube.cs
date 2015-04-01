891d1928ead6d6e3f5c6eedadac226429d61b63c
Christian Diaz <cdiaz8369@hotmail.com>
2015-04-01 01:26:00 -0400

    Nav Mesh-2
    -Edited FollowCube.cs to have animation transitions
    -Pathfinding is working, with a few kinks.

--- Darknet/Assets/FollowCube.cs
+++ Darknet/Assets/FollowCube.cs
@@ -5,15 +5,52 @@ public class FollowCube : MonoBehaviour {
 
 	public Transform target;
 	public float zOffset;
+	
+	private Animator animator;
+	public NavMeshAgent myAgent;
+
 	// Use this for initialization
 	void Start () {
+	
+		animator = GetComponent<Animator>();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	if (myAgent.velocity.x == 0){
+		animator.SetInteger("Direction", 0);
 	}
+	else if (myAgent.velocity.z > .5f){
+		animator.SetInteger("Direction", 8);
+	}
+	else if (myAgent.velocity.z < -.5f){
+		animator.SetInteger("Direction", 5);
+	}
+	else if (myAgent.velocity.x > .5f){
+		animator.SetInteger("Direction", 6);
+	}
+	else if (myAgent.velocity.x < -.5f){
+		animator.SetInteger("Direction", 4);
+	}
+
+	
+	else if (myAgent.velocity.x < -.5f && myAgent.velocity.z > .5f){
+		animator.SetInteger("Direction", 7);
+	}
+	else if (myAgent.velocity.x > .5f && myAgent.velocity.z > .5f){
+		animator.SetInteger("Direction", 9);
+	}
+	else if (myAgent.velocity.x < -.5f && myAgent.velocity.z < -.5f){
+		animator.SetInteger("Direction", 1);
+	}
+	else if (myAgent.velocity.x > .5f && myAgent.velocity.z < -.5f){
+		animator.SetInteger("Direction", 3);	
+	
+	}
+
+}
 
 	void LateUpdate(){
 		transform.localPosition = new Vector3 (target.localPosition.x, transform.localPosition.y, target.localPosition.z+ zOffset);
