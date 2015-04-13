51fafff1c2a6f920bb9f051b0bc19f0296424c2d
Christian Diaz <cdiaz8369@hotmail.com>
2015-04-12 21:03:59 -0400

    Attempted Transition
    -Added scripts for transisition
    -Added 3D world panel objects, cube for testing

---
+++ Darknet/Assets/Scripts/Transport Scripts/Trans_12_11.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trans_12_11 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Darknet/Assets/Scripts/Transport Scripts/Trans_9_10.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trans_9_10 : MonoBehaviour {
+
+	public Transform target;  
+	
+	void OnTriggerEnter(Collider other){
+		target.position = new Vector3(-15, 1, -2);		
+	}	
+}
