edf9b06a118cc326fae45019955843b853f95c7d
Cammie Storey <cammiestorey@gmail.com>
2015-05-10 14:33:26 -0400

    Re-Added My Scripts

---
+++ Darknet/Assets/ExpBarLabelLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+//Cammie Storey
+
+public class ExpBarLabelLevel : MonoBehaviour {
+
+	[SerializeField] public Text label;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.FindWithTag ("Player");
+		Actor actor = player.GetComponent<Actor> ();
+
+		label.text = "Level " + actor.getAttribute ("lv").ToString();
+
+	
+	}
+}
