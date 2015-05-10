82cd09e69faddd9871ec590fcc833d09c5364a88
Christian Diaz <cdiaz8369@hotmail.com>
2015-05-10 19:21:40 -0400

    Several Additions
    -Added Pathfinding over all areas*
    -Added to player prefab and Drkntslv objects
    *Only tested in town and arena but should work in all in      theory
    -Added DGB assets
    -Changed Townzone pillar to DGB statue

--- Darknet/Assets/Scripts/UI/DetectKeys.cs
+++ Darknet/Assets/Scripts/UI/DetectKeys.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 
 public class DetectKeys : MonoBehaviour {
 	[SerializeField] public string slotName;
-	[SerializeField] public UISpellSlot parentSlot;
+//	[SerializeField] public UISpellSlot parentSlot;
 
 	// Use this for initialization
 	void Start (){
