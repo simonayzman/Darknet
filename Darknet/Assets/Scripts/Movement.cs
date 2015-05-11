19d015a2499f1c0ddfa4f13ac76c94e8c6759cc6
Simon Ayzman <simonayzman@gmail.com>
2015-05-11 11:35:59 -0400

    Minor changes

--- Darknet/Assets/Scripts/Movement.cs
+++ Darknet/Assets/Scripts/Movement.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 	public float speed;
+
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
+	
+		animator = GetComponent<Animator>();
 	
 	}
 	
@@ -13,51 +18,73 @@ public class Movement : MonoBehaviour {
 
 		//modified conditions for built in key map. default keys for axes are wasd and arrows.
 
+		animator.SetInteger("Direction", 0);
 
+
+		if (Input.GetAxisRaw ("Horizontal") > 0 && Input.GetAxisRaw ("Vertical") > 0)
+		{
+		    animator.SetInteger("Direction", 9);
+			transform.position += new Vector3 (speed * Time.deltaTime, speed * Time.deltaTime, 0.0f);
+		}
+		//if (Input.GetKey (KeyCode.A))
+		/*4*/
+		if (Input.GetAxisRaw ("Horizontal") > 0 && Input.GetAxisRaw ("Vertical") < 0)
+		{
+			animator.SetInteger("Direction", 2);
+			transform.position -= new Vector3 (speed * Time.deltaTime, speed * Time.deltaTime, 0.0f);
+		}
+		//if (Input.GetKey (KeyCode.W))
+		/*8*/	
+		if (Input.GetAxisRaw ("Horizontal") < 0 && Input.GetAxisRaw ("Vertical") > 0)
+		{
+			animator.SetInteger("Direction", 7);
+			transform.position += new Vector3 (-speed * Time.deltaTime, speed * Time.deltaTime, 0.0f);
+		}
+		//if (Input.GetKey (KeyCode.S))
+		/*5*/
+		if (Input.GetAxisRaw ("Horizontal") < 0 && Input.GetAxisRaw ("Vertical") < 0){
+			animator.SetInteger("Direction", 1);
+			transform.position -= new Vector3 (speed * Time.deltaTime, -speed * Time.deltaTime, 0.0f);
+		}
 
 
 
 
 
 		//if (Input.GetKey (KeyCode.D))
+		/*6*/
 		if (Input.GetAxisRaw ("Horizontal") > 0)
+		{
+		    animator.SetInteger("Direction", 6);
 			transform.position += new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+		}
 		//if (Input.GetKey (KeyCode.A))
+		/*4*/
 		if (Input.GetAxisRaw ("Horizontal") < 0)
+		{
+			animator.SetInteger("Direction", 4);
 			transform.position -= new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+		}
 		//if (Input.GetKey (KeyCode.W))
+		/*8*/	
 		if (Input.GetAxisRaw ("Vertical") > 0)
+		{
+			animator.SetInteger("Direction", 8);
 			transform.position += new Vector3 (0.0f, speed * Time.deltaTime, 0.0f);
+		}
 		//if (Input.GetKey (KeyCode.S))
-		if (Input.GetAxisRaw ("Vertical") < 0)
+		/*5*/
+		if (Input.GetAxisRaw ("Vertical") < 0){
+			animator.SetInteger("Direction", 5);
 			transform.position -= new Vector3 (0.0f, speed * Time.deltaTime, 0.0f);
-	
+		}
 
 	}
 
-	public void Update() {
-		
-		//modified conditions for built in key map. default keys for axes are wasd and arrows.
-		
-		
-		
-		
-		
-		
-		
-		//if (Input.GetKey (KeyCode.D))
-		if (Input.GetAxisRaw ("Horizontal") > 0)
-			transform.position += new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
-		//if (Input.GetKey (KeyCode.A))
-		if (Input.GetAxisRaw ("Horizontal") < 0)
-			transform.position -= new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
-		//if (Input.GetKey (KeyCode.W))
-		if (Input.GetAxisRaw ("Vertical") > 0)
-			transform.position += new Vector3 (0.0f, speed * Time.deltaTime, 0.0f);
-		//if (Input.GetKey (KeyCode.S))
-		if (Input.GetAxisRaw ("Vertical") < 0)
-			transform.position -= new Vector3 (0.0f, speed * Time.deltaTime, 0.0f);
-		
-		
-	}
 }
+
+
+
+
+
+
